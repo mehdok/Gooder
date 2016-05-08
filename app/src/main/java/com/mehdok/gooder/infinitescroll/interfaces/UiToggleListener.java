a388b94c79f06b9a6ed26728afb0f1cb540562f6
mehdok <mehdok@gmail.com>
2016-05-08 16:14:18 +0430

    Hide Fab on scroll.

---
+++ app/src/main/java/com/mehdok/gooder/infinitescroll/interfaces/UiToggleListener.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2016. Mehdi Sohrabi
+ */
+
+package com.mehdok.gooder.infinitescroll.interfaces;
+
+/**
+ * Created by mehdok on 5/8/2016.
+ */
+public interface UiToggleListener {
+    void show();
+
+    void hide();
+}
